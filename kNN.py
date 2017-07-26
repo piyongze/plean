601f5e0b824ebdcc9d1b791d8c2d2bdc141776f3
piyongze <piyongze@ymatou.com>
2017-07-26 19:52:41 +0800

    'all'

--- kNN.py
+++ kNN.py
@@ -3,48 +3,90 @@ import operator
 import matplotlib
 import matplotlib.pyplot as plt
 
+
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group,labels
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
-    print(diffMat)
-    sqDiffMat = diffMat**2
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
-    print(distances)
     sortedDistIndicies = distances.argsort()
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
 
 def file2matrix(filename):
     fr = open(filename)
     arrayOLines = fr.readlines()
     numberOfLines = len(arrayOLines)
-    returnMat = zeros((numberOfLines,3))
+    returnMat = zeros((numberOfLines, 3))
     classLabelVector = []
     index = 0
     for line in arrayOLines:
         line = line.strip()
         listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
+        returnMat[index, :] = listFromLine[0:3]
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
-    return returnMat,classLabelVector
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 4)
+        print("the classifier came back with: %d, the real answer is : %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print("the total error rate is : %f" % (errorCount / float(numTestVecs)))
+
+
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(input("percentage of time spend playing video games?"))
+    ffMiles = float(input("frequent filer miles earned per year?"))
+    iceCream = float(input("ice cream per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges , minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("you will probably like this person: ", resultList[classifierResult - 1])
+
 
 
 if __name__ == '__main__':
-    group,labels = createDataSet()
-    res = classify0([0,0], group, labels, 3)
-    returnMat,classLabelVector =  file2matrix('datingTestSet.txt')
+    group, labels = createDataSet()
+    res = classify0([0, 0], group, labels, 3)
+    returnMat, classLabelVector = file2matrix('datingTestSet.txt')
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(returnMat[:,0],returnMat[:,2],15.0*array(classLabelVector),15.0*array(classLabelVector))
+    ax.scatter(returnMat[:, 0], returnMat[:, 1], 15.0 * array(classLabelVector), 15.0 * array(classLabelVector))
     plt.show()
+    datingClassTest()
+    classifyPerson()

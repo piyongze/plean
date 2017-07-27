ff9b5c513735ce2009bf063db826c23bd2feadea
piyongze <piyongze@ymatou.com>
2017-07-27 10:13:50 +0800

    'complete_tree'

--- tree.py
+++ tree.py
@@ -1,9 +1,11 @@
 from math import log
+import operator
 
 """
 sum(-p*log(p,2)) 信息熵
 不确定性越大 信息熵越大 
 """
+
 
 def calcShannonEnt(dataSet):
     """
@@ -20,7 +22,7 @@ def calcShannonEnt(dataSet):
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
-        prob = float(labelCounts[key])/numEntries
+        prob = float(labelCounts[key]) / numEntries
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
 
@@ -30,16 +32,16 @@ def createDataSet():
     测试数据集
     :return:
     """
-    dataSet = [[1,1,'yes'],
-               [1,1,'yes'],
-               [1,0,'no'],
-               [0,1,'no'],
-               [0,1,'no']]
-    labels = ['no surfacing','flippers']
-    return dataSet,labels
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
 
 
-def splitDatase(dataSet, axis, value):
+def splitDataSet(dataSet, axis, value):
     """
     使用特征值分割数据集 以便计算信息熵和信息增益
     :param dataSet:
@@ -51,7 +53,7 @@ def splitDatase(dataSet, axis, value):
     for featVec in dataSet:
         if featVec[axis] == value:
             reducedFeatVec = featVec[:axis]
-            reducedFeatVec.extend(featVec[axis+1:])
+            reducedFeatVec.extend(featVec[axis + 1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
 
@@ -64,24 +66,95 @@ def chooseBestFeatureToSplit(dataSet):
     """
     numFeatures = len(dataSet[0]) - 1
     baseEntropy = calcShannonEnt(dataSet)
-    bestInfoGain = 0.0; bestFeature = -1
+    bestInfoGain = 0.0;
+    bestFeature = -1
     for i in range(numFeatures):
         featList = [example[i] for example in dataSet]
         uniqueVals = set(featList)
-        newEntropy  = 0.0
+        newEntropy = 0.0
         for value in uniqueVals:
-            subDataSet = splitDatase(dataSet,i,value)
-            prob = len(subDataSet)/float(len(dataSet))
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)
         infoGain = baseEntropy - newEntropy
-        if(infoGain > bestInfoGain):
+        if (infoGain > bestInfoGain):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
 
 
+def majorityCnt(classList):
+    """
+    投票表决 最后的未分类节点
+    :param classList:
+    :return:
+    """
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
-myDat,labels = createDataSet()
-print(splitDatase(myDat,0,1))
-print(chooseBestFeatureToSplit(myDat))
+def createTree(dataSet, labels):
+    """
+    创建决策树
+    :param dataSet:
+    :param labels:
+    :return:
+    """
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    """
+    使用决策树分类
+    :param inputTree:
+    :param featLabels:
+    :param testVec:
+    :return:
+    """
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+
+myDat, labels = createDataSet()
+print(myDat)
+print(labels)
+# print(splitDataSet(myDat, 0, 1))
+# print(chooseBestFeatureToSplit(myDat))
+myTree = createTree(myDat, labels)
+myDat, labels = createDataSet()
+print(myTree)
+print(classify(myTree, labels, [1, 0]))
+print(classify(myTree, labels, [1, 1]))
+
+
+fr = open('lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age','prescript','astigmatic','tearRate']
+lensesTree = createTree(lenses,lensesLabels)
+print(lensesTree)
